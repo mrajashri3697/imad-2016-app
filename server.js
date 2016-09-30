acd6b8d90673a70e21c7b8e5eafa347b4c08cd5e
nikitha3697 <nikitha3697@gmail.com>
2016-09-30 21:43:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("ARTICLE ONE REQUESTED WILL BE SERVED!!!");
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 }); 
 app.get('/article-two', function (req, res) {
   res.send("ARTICLE TWO REQUESTED WILL BE SERVED!!!");
